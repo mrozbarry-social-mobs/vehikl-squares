dc31993ed61c01d35452296503f336122e8989ca
Alex Barry <alex.barry@gmail.com>
2020-08-25 21:04:18 +0000

    Render vehikl squares
     - Tailwind is apparently not working?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import VehiklSquares from './components/VehiklSquares'
 
 function App() {
   return (
     <div className="App">
       <p>Awesome tic-tac-toe game</p>  
+        <VehiklSquares/>
     </div>
+
   );
 }
 
--- src/components/VehiklSquares.js
+++ src/components/VehiklSquares.js
@@ -3,8 +3,7 @@ import React from 'react';
 function VehiklSquares(){
 
   return(
-    <div>
-      Vehikl Squares
+    <div className="grid grid-flow-col grid-cols-3 grid-rows-3 gap-3">
       <div data-testid="cell">taco</div>
       <div data-testid="cell">taco</div>
       <div data-testid="cell">taco</div>
